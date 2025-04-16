14bb09eff2444d50af3a01d29682d33c710ffd64
lcletz <laura@cletz.fr>
2025-04-16 21:09:33 +0200

    zzz reset

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,14 @@
 .onLoad <- function(libname, pkgname) {
 
-# Checking if there are already loaded grids in the package environment
+  # Checking if there are already loaded grids in the package environment
   if (!exists("takuzu_grids", envir = asNamespace(pkgname))) {
 
     grids <- dl_csv()  # load grids via dl_csv()
 
-# save them in the hidden package environment
+    # save them in the hidden package environment
     assign("takuzu_grids", grids, envir = asNamespace(pkgname))
 
-# cache on disk for future sessions
+    # cache on disk for future sessions
     cache_dir <- tools::R_user_dir("TakuzuKL", which = "cache")
     if (!dir.exists(cache_dir)) dir.create(cache_dir, recursive = TRUE)
     saveRDS(grids, file.path(cache_dir, "grids_cache.rds"))
@@ -21,5 +21,5 @@
 #'
 #' @export
 get_grids <- function() {
-get("takuzu_grids", envir = asNamespace("TakuzuKL"))
+  get("takuzu_grids", envir = asNamespace("TakuzuKL"))
 }
